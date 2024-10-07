d8c4ec819ccd63c0e80119d59a6d58bd020c8e36
pzhwuhu <23373390@buaa.edu.cn>
2024-10-07 22:16:13 +0800

    Site updated: 2024-10-07 22:16:12

--- js/duration.js
+++ js/duration.js
@@ -22,7 +22,7 @@
         if (String(snum).length === 1) {
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "本小站已稳定运行&nbsp" + dnum + "&nbsp天";
+        document.getElementById("timeDate").innerHTML = "小破站在风雨中已顽强运行&nbsp" + dnum + "&nbsp天";
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
 
